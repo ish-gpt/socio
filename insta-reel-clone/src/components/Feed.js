16794069a67cb9e78ac1488a13cd9c806879e1d1
ishan.gupta <ishan.gupta@intimetec.com>
2024-10-20 14:00:13 +0530

    Did Feed css and implemented post save feature in firestore

--- insta-reel-clone/src/components/Feed.js
+++ insta-reel-clone/src/components/Feed.js
@@ -1,25 +1,260 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { AuthContext } from '../context/AuthContext';
-// import { useContext } from 'react';
+import './Feed.css';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import insta from '../images/ig-logo.png';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import ExploreOutlinedIcon from '@mui/icons-material/ExploreOutlined';
+import SlideshowOutlinedIcon from '@mui/icons-material/SlideshowOutlined';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import Suggestion from './Suggestion';
+import phoneInsta from '../images/phone-insta.jpg';
+import profile1 from '../images/profile-3.jpeg';
+import profile2 from '../images/profile-5.jpeg';
+import profile3 from '../images/profile-4.jpeg';
+import profile4 from '../images/profile-6.jpeg';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import LinearProgress from '@mui/joy/LinearProgress';
+import Snackbar from '@mui/joy/Snackbar';
+import { uploadUserPosts } from '../profileStorage';
+import { getUserData } from '../firebaseCRUD';
+
+
+const drawerWidth = 280;
 
 export default function Feed() {
-    let { logout } = useContext(AuthContext);
 
+    // let uploadSuccessfull;
+    let IconsArr = [<HomeIcon />, <SearchIcon />, <ExploreOutlinedIcon />, <></>, <ChatBubbleOutlineIcon />, <FavoriteBorderIcon />, <AddBoxOutlinedIcon />, <AccountCircleOutlinedIcon />]
+    let { logout, user } = useContext(AuthContext);
+    const [file, setFile] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [userDetails, setUserDetails] = useState(false);
+    const [open, setOpen] = useState(true)
+    const [error, setError] = useState('');
+    const [errorTimer, setErrorTimer] = useState(0);
+    const [uploadTimer, setUploadTimer] = useState(0);
+    const [uploadSuccessfull, setUploadSuccessfull] = useState(true);
+    const storiesArray = [profile1, profile2, profile3, profile4, phoneInsta, profile2, profile1, profile4, phoneInsta, profile2, profile3, phoneInsta, profile2, profile3, profile4, phoneInsta, profile2, profile1, phoneInsta, profile2, profile4, profile3, profile3, profile3, profile3]
 
-    // console.log(x);
+    useEffect(() => {
+        getUserData(user).then((res) => {
+            setUserDetails(res);
+        });
+        // return () => {unsub()}
+    }, [user])
 
     async function logOut() {
         await logout();
     }
 
+    async function handleUploadReel(file) {
+        setLoading(true);
+        if (file == null) {
+            setError('Please Select File First');
+            setErrorTimer(5000);
+            setTimeout(() => {
+                setError('');
+                setErrorTimer(0);
+                setOpen(true);
+            }, 5000);
+            return;
+        }
+        if (((file.size) / (1024 * 1024)) > 100) {
+            setError('File Should be < 100MB');
+            setErrorTimer(5000);
+            setTimeout(() => {
+                setError('');
+                setErrorTimer(0);
+                setOpen(true);
+            }, 5000);
+            return;
+        }
+
+        try {
+            await uploadUserPosts(userDetails, file).then(() => {
+                setLoading(false);
+                setUploadTimer(5000);
+                setUploadSuccessfull(true);
+                //post upload successfull notification
+            }).catch((error) => {
+                setLoading(false);
+                setError('Error in Uploading Reel', error);
+                setErrorTimer(5000);
+                setTimeout(() => {
+                    setError('');
+                    setErrorTimer(0);
+                    setOpen(true);
+                }, 5000);
+            });
+        } catch (error) {
+            setLoading(false);
+            setError('Error in Uploading Reel');
+            setErrorTimer(5000);
+            setTimeout(() => {
+                setError('');
+                setErrorTimer(0);
+                setOpen(true);
+            }, 5000);
+        }
+
+    }
     return (
-        <>
-            <div>
-                <p>Welcome to Feed</p>
-            </div>
-            <div>
-                <button onClick={logOut}>Logout</button>
-            </div>
-        </>
-    )
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar style={{ color: 'black', backgroundColor: 'white' }}
+                position="fixed"
+                sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
+            >
+                <div className='feed-header'>
+                    <div>
+                        <Toolbar>
+                            <Typography variant="h6" noWrap component="div">
+                                <Stack direction="row" spacing={2} maxWidth={720} sx={{
+                                    overflowX: 'hidden', // Hide overflow (both X and Y) by default
+                                    '&:hover': {
+                                        overflow: 'auto', // Show overflow when hovering over the Stack
+                                        cursor: 'pointer'
+                                    },
+                                    '&::-webkit-scrollbar': {
+                                        display: 'none'
+                                    }
+                                }}>
+                                    {
+                                        storiesArray.map((imgSrc) => (
+                                            <div style={{
+                                                display: 'inline-block',
+                                                padding: '3px', // Padding creates space for the border
+                                                background: 'linear-gradient(to right, red, purple)',
+                                                borderRadius: '50%', // Full border radius for a circular shape
+                                            }}>
+                                                <Avatar alt="" src={imgSrc} sx={{
+
+                                                    borderRadius: '50%', // Ensure the Avatar remains circular
+                                                    backgroundColor: 'white', // Optional: set a background for the inner avatar
+                                                }} />
+                                            </div>
+                                        ))
+                                    }
+                                </Stack>
+                            </Typography>
+                        </Toolbar>
+                    </div>
+                    <div>
+                        <Suggestion></Suggestion>
+                    </div>
+                </div>
+
+            </AppBar>
+            <Drawer
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        width: drawerWidth,
+                        boxSizing: 'border-box',
+                    },
+                }}
+                variant="permanent"
+                anchor="left"
+            >
+                <Toolbar>
+                    <div className='insta-image-wrapper'>
+                        <img className='ig-logo' src={insta} alt='ig-logo'></img>
+                    </div>
+                </Toolbar>
+                {/* <Divider /> */}
+                <List>
+                    {['Home', 'Search', ' Explore', 'Reel', 'Messages', 'Notification', 'Create', 'Profile'].map((text, index) => (
+                        <ListItem sx={{ marginBottom: 2 }} key={text} disablePadding>
+                            <ListItemButton component='label'>
+                                <ListItemIcon style={{ color: 'black' }}>
+                                    {text == 'Reel' ?
+                                        <>
+                                            <SlideshowOutlinedIcon />
+                                            <input type="file" accept='video/*' hidden onChange={(e) => handleUploadReel(e.target.files[0])} />
+                                        </>
+                                        : IconsArr[index]}
+                                </ListItemIcon>
+                                <ListItemText primary={text} />
+                            </ListItemButton>
+                            {(text == 'Reel' && (loading == true)) ? <LinearProgress
+                                color="primary"
+                                size="sm"
+                                value={30}
+                                variant="outlined"
+                                thickness={3}
+                            /> : <></>}
+                        </ListItem>
+
+                    ))}
+                </List>
+            </Drawer>
+            <Box
+                component="main"
+                sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
+            >
+                <Toolbar />
+                <Typography sx={{ marginBottom: 2 }}>
+                    <h1>POST-1</h1>
+                </Typography>
+                <Typography sx={{ marginBottom: 2 }}>
+                    <h1>POST-2</h1>
+                    <button onClick={logOut}>Logout</button>
+                </Typography>
+            </Box>
+            {
+                error != '' ? <Snackbar
+                    autoHideDuration={errorTimer}
+                    open={open}
+                    color='danger'
+                    variant='solid'
+                    onClose={(event, reason) => {
+                        if (reason === 'clickaway') {
+                            return;
+                        }
+                        setOpen(false);
+                    }}
+                >
+                    {error}
+                </Snackbar> : <></>
+            }
+            {
+                uploadSuccessfull? <Snackbar
+                    autoHideDuration={uploadTimer}
+                    open={uploadSuccessfull}
+                    color='success'
+                    variant='solid'
+                    onClose={(event, reason) => {
+                        if (reason === 'clickaway') {
+                            return;
+                        }
+                        // uploadSuccessfull = false;
+                        setUploadSuccessfull(false);
+                    }}
+                >
+                    Post uploaded Successfully
+                </Snackbar> :<></>
+            }
+
+        </Box>
+
+    );
 }
--- insta-reel-clone/src/profileStorage.js
+++ insta-reel-clone/src/profileStorage.js
@@ -1,5 +1,6 @@
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import { add } from "./firebaseCRUD";
+import { add, addPostsToDb } from "./firebaseCRUD";
+const { v4: uuidv4 } = require('uuid');
 
 export async function uploadUserProfile(user,file, userName) {
 
@@ -7,14 +8,8 @@ export async function uploadUserProfile(user,file, userName) {
     const storageRef = ref(storage, `/users/${user.uid}/ProfileImage`);
     const uploadTask = uploadBytesResumable(storageRef, file);
 
-    // Register three observers:
-    // 1. 'state_changed' observer, called any time the state changes
-    // 2. Error observer, called on failure
-    // 3. Completion observer, called on successful completion
     uploadTask.on('state_changed',
         (snapshot) => {
-            // Observe state change events such as progress, pause, and resume
-            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
             const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
             console.log('Upload is ' + progress + '% done');
             switch (snapshot.state) {
@@ -31,16 +26,58 @@ export async function uploadUserProfile(user,file, userName) {
             throw new Error('Error Uploading Profile Picture');
         },
         async () => {
-            // Handle successful uploads on complete
-            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
             let downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
             let data = {
                 downloadURL: downloadURL,
                 fullName: userName
             }
-            // console.log('File available at', downloadURL);
-            // downloadUrl = downloadURL;
             await add(user, "users", data)
         }
     );
+}
+
+
+
+export async function uploadUserPosts(user, file) {
+    return new Promise((resolve,reject) => {
+
+        const storage = getStorage();
+        const uid = uuidv4();
+        const storageRef = ref(storage, `/posts/${uid}/${file.name}`);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+
+        uploadTask.on('state_changed',
+            (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log('Post Upload is ' + progress + '% done');
+                switch (snapshot.state) {
+                    case 'paused':
+                        console.log('Upload is paused');
+                        break;
+                    case 'running':
+                        console.log('Upload is running');
+                        break;
+                }
+            },
+            (error) => {
+                console.log('Error Uploading Post', error);
+                reject(new Error('Error Uploading Post'));
+            },
+            async () => {
+                let downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+                let data = {
+                    likes: [],
+                    comments: [],
+                    pId: uid,
+                    pURL: downloadURL,
+                    userID: user.uid,
+                    userProfile: user.profileURL,
+                    uName: user.fullName
+                }
+                await addPostsToDb(user, "posts", data)
+                resolve();
+            }
+        );
+    })
 }
