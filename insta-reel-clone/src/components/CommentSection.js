916c0c68210e8043d8f8a37464db23a3ef1796e8
ishan.gupta <ishan.gupta@intimetec.com>
2024-11-16 22:02:19 +0530

    Implementd add comment to a post and like unlike a comment

--- insta-reel-clone/src/components/CommentSection.js
+++ insta-reel-clone/src/components/CommentSection.js
@@ -1,126 +1,134 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import { AuthContext } from '../context/AuthContext';
 import './CommentSection.css';
 import Actions from './Actions';
 import TurnedInNotIcon from '@mui/icons-material/TurnedInNot';
-import BookmarkIcon from '@mui/icons-material/Bookmark';
 import TextField from '@mui/material/TextField';
 import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+import CircularProgress from '@mui/material/CircularProgress';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import { addCommentToDb, getCommentsFromDb } from '../firebaseCRUD';
+import { getUserData } from '../firebaseCRUD';
+import LikeComment from './LikeComment';
+
 
 export default function CommentSection({ userData, postData }) {
-    return (
-        <div className='comments-wrapper'>
-            <div className='comments-nav'>
-                <div className='profile-wrapper'>
-                    <img className='user-profile' src={postData.userProfile}></img>
-                </div>
-                <div style={{ placeContent: 'center' }}>
-                    <h3>{
-                        postData.uName
-                    }</h3>
-                </div>
-            </div>
-            <div className='comments-content'>
-                <div className='comments'>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                    mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm<br></br>
-                </div>
-            </div>
-            <div className='comments-actions'>
-                <div className='comment-icons'>
-                    <div className='like-comment-share'>
-                        <Actions userData={userData} postData={postData}></Actions>
-                    </div>
-                    <div className='save-icon'>
-                        <TurnedInNotIcon></TurnedInNotIcon>
-                    </div>
-                </div>
-                <div className='likes-section'>
-                    <strong>1,324 Likes</strong>
-                </div>
-                <div className='comment-text-box'>
-                    <TextField id="outlined-basic" fullWidth label="Comment" variant="outlined"
-                        slotProps={{
-                            input: {
-                                startAdornment: <InsertEmoticonIcon position="start">kg</InsertEmoticonIcon>,
-                                endAdornment:<>Post</>
-                            },
-                        }}
-                    >
-                    </TextField>
-                </div>
-            </div>
-        </div>
-    )
+	const [postComments, setPostComments] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [addCommentLoader, setAddCommentLoader] = useState(false);
+	const [comment, setComment] = useState('');
+	let { user } = useContext(AuthContext);
+	const [userDetails, setUserDetails] = useState(false);
+	useEffect(() => {
+		setLoading(true);
+		getUserData(user).then(async (res) => {
+			let result = await fetchComments();
+			setUserDetails(res);
+			setPostComments(result);
+			setLoading(false);
+		});
+	}, [user]);
+
+	const fetchComments = async () => {
+		const commentsArr = await Promise.all(postData.comments.map(async (comntId) => {
+			let x = await getCommentsFromDb(comntId);
+			return x;
+		}));
+
+		return commentsArr;
+		
+	};
+
+	
+
+	async function handleCommentSubmit() {
+		setAddCommentLoader(true);
+		let commentData = {
+			postId: postData.postId,
+			comment: comment,
+			userProfileImage: userDetails.profileURL,
+			userName: userDetails.fullName,
+			likedBy:[]
+		}
+		await addCommentToDb(commentData, 'comments', commentData.postId);
+		setComment('');
+		const commentsList = [...postComments, commentData];
+		setPostComments(commentsList);
+		setAddCommentLoader(false);
+	}
+
+	return (
+		<div className='comments-parent-wrapper'>
+			<div className='comments-nav'>
+				<div className='profile-wrapper'>
+					<Avatar
+						alt="Remy Sharp"
+						src={postData.userProfile}
+						sx={{ width: 62, height: 62 }}
+					/>
+				</div>
+				<div style={{ placeContent: 'center' }}>
+					<h3>{
+						postData.uName
+					}</h3>
+				</div>
+			</div>
+			<div className='comments-content'>
+				<div className='comments'>
+					{
+						loading == true ? <div className='loader'><CircularProgress /></div> : <>
+							{
+								postComments && postComments.map((comment) => (
+									<>
+										{
+											<div className='cmnt-div'>
+												<div>
+													<Avatar alt="Remy Sharp" src={comment.userProfileImage} />
+												</div>
+												<div className='comment-wrapper'>
+													<div className='user-name-comment'>
+														<strong style={{ marginRight: '0.5em' }}>{comment.userName}</strong>
+														<label>{comment.comment}</label>
+													</div>
+													<div className='like-comment'>
+														{userDetails != false && <LikeComment commentData={comment} userDetails={userDetails}></LikeComment>}
+													</div>
+												</div>
+											</div>
+										}
+									</>
+								))
+							}
+							{addCommentLoader && <div className='loader'><CircularProgress size='15px' /></div>}
+						</>
+					}
+				</div>
+			</div>
+			<div className='comments-actions'>
+				<div className='comment-icons'>
+					<div className='like-comment-share'>
+						<Actions userData={userData} postData={postData}></Actions>
+					</div>
+					<div className='save-icon'>
+						<TurnedInNotIcon></TurnedInNotIcon>
+					</div>
+				</div>
+				<div className='likes-section'>
+					<strong>1,324 Likes</strong>
+				</div>
+				<div className='comment-text-box'>
+					<TextField id="outlined-basic" fullWidth label="Comment" value={comment} variant="outlined" onChange={(e) => { setComment(e.target.value) }}
+						slotProps={{
+							input: {
+								startAdornment: <InsertEmoticonIcon position="start"></InsertEmoticonIcon>,
+								endAdornment: <Button onClick={() => handleCommentSubmit()} variant="text" disabled={false}>Post</Button>
+							},
+						}}
+					>
+					</TextField>
+				</div>
+			</div>
+		</div>
+	)
 }
--- insta-reel-clone/src/firebaseCRUD.js
+++ insta-reel-clone/src/firebaseCRUD.js
@@ -51,3 +51,65 @@ export async function addPostsToDb(user, dbName, data) {
         console.log("errro occured while uploading posts to db", error);
     }
 }
+
+export async function addCommentToDb(commentData, dbName, postId) {
+    try {
+        const docRef = await addDoc(collection(dbInstance, dbName), {
+            ...commentData
+        });
+        const postDocRef = doc(dbInstance, 'posts', postId);
+        await updateDoc(postDocRef, {
+            comments: arrayUnion(docRef.id)
+        });
+        console.log("comments saved to db successfull");
+    } catch(error) {
+        console.log("error occured while saving comment to db", error);
+    }
+}
+
+export async function getCommentsFromDb(commentId) {
+    try {
+        const docRef = doc(dbInstance, 'comments', commentId);
+        const docSnap = await getDoc(docRef);
+        
+        if (docSnap.exists()) {
+            // console.log("---", docSnap.id)
+            return { ...docSnap.data(), commentId:docSnap.id};
+        } else {
+            console.log("No such document!");
+            throw "Error fetching comments"
+        }
+    } catch (error) {
+        console.log("Error",error);
+    }
+}
+
+export async function updateCommentData(commentData, isLiked, userDetails) {
+    try {
+        const commentRef = doc(dbInstance, 'comments', commentData.commentId);
+        const docSnap = await getDoc(commentRef);
+        let data = await Promise.all([docSnap.data()]);
+        let likesArr = []
+        if (!isLiked) {
+            likesArr = [...data[0].likedBy, userDetails.uid];            
+        } else {
+            likesArr = data[0].likedBy.filter((likeID) => {
+                return likeID != userDetails.uid
+            });
+        }
+        await updateDoc(commentRef, {
+            likedBy: likesArr
+        });
+        console.log('comments updated successful');
+    } catch (error) {
+        console.log("Error", error);
+    }
+}
+
+
+export async function isCommentLikedByUser(commentData, userDetails) {
+    const commentRef = doc(dbInstance, 'comments', commentData.commentId);
+    const docSnap = await getDoc(commentRef);
+    let data = await Promise.all([docSnap.data()]);
+    return data[0].likedBy.includes(userDetails.uid);
+}
